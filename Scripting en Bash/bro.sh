d8abe78843c644c5f6a315d0e479a126f13fde87
JaRoCal <124217106+JaRoCal@users.noreply.github.com>
2023-05-25 21:35:32 -0600

    Add files via upload

---
+++ Scripting en Bash/bro.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#Script bro.sh
+#Jaime Ronaldo Calderon Sanchez 1970947
+
+read -p "Escribe tu nombre: " nombre
+echo "Hola, $nombre. Seamos amigos!"
---
+++ Scripting en Bash/welcome.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#Script welcome.sh
+#Jaime Ronaldo Calderon Sanchez 1970947
+echo "Hola ${LOGNAME}"
+echo "Hoy es $(date)"
+echo "Usuarios actuales conectados, y sus procesos:"
+w
